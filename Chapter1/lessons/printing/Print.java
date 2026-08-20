3108effdc2b8e621693023248dbd6291ed831321
ssbodkhe <ssbodkhe@stu.naperville203.org>
2026-08-20 14:01:23 -0500

    Added printing program

---
+++ Chapter1/lessons/printing/Print.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Print here.
+ *
+ * @author (Shourya B)
+ * @version (8/20/26)
+ */
+public class Print
+{
+    public static void main(String[] args) {
+            // a single line comment
+            
+            /* a
+             * multi 
+             * line
+             * comment
+             */
+            System.out.println("Hello, World!"); 
+        }
+
+}
