3d0d2912f7b9ef23fb4bf2dd629f71d009fd9776
ssbodkhe <ssbodkhe@stu.naperville203.org>
2026-08-25 14:04:54 -0500

    formatted

--- Chapter1/lessons/Budget/WeeklyBudget.java
+++ Chapter1/lessons/Budget/WeeklyBudget.java
@@ -1,9 +1,10 @@
 /**
  * Weekly Budget
  * Shourya Bodkhe
- * Last Updated: 8/24/26
+ * Last Updated: 8/25/26
  */
 import java.util.Scanner;
+import java.text.NumberFormat;
 public class WeeklyBudget
 {
     //Calculate weekly lunch spending and remaining money
@@ -11,6 +12,8 @@ public class WeeklyBudget
         String name;
         double allowance, lunchPrice, totalCost, remaining;
         int lunchesPerWeek;
+        
+        NumberFormat money = NumberFormat.getCurrencyInstance();
         
         // Instantiate the Scanner obj to read from the keyboard
         Scanner scan = new Scanner(System.in);
@@ -34,9 +37,13 @@ public class WeeklyBudget
         totalCost = lunchPrice * lunchesPerWeek;
         remaining = allowance - totalCost;
         
-        System.out.println(allowance);
-        System.out.println(totalCost);
-        System.out.println(remaining);
+        // printf allows placeholders for strings using %s
+        // %n adds new line
+        System.out.printf("%n--- Weekly Budget Summary for %s ---%n", name);
+        
+        System.out.printf("%-25s %s%n", "Weekly Allowance:", money.format(allowance));
+        System.out.printf("%-25s %s%n", "Total Spent on lunches:", money.format(totalCost));
+        System.out.printf("%-25s %s%n", "Money remaining:", money.format(remaining));
         
         
         
